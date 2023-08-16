8a88c62eca737b3fa11cb08e44a2b616fec4b54c
Arseny <20mineslend14@gmail.com>
2023-08-16 15:07:24 +0500

    init project

---
+++ src/main/java/com/example/post/PostTransportationApplication.java
@@ -0,0 +1,13 @@
+package com.example.post;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostTransportationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostTransportationApplication.class, args);
+    }
+
+}
